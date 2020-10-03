725496c968db8b5141e50c2bd8516524a1d92178
Mars chen <chenzihao@bupt.edu.cn>
2020-10-03 16:32:15 +0800

    tested code find a bug the dummy node caused

--- lab0/cprogramminglab-handout/queue.c
+++ lab0/cprogramminglab-handout/queue.c
@@ -26,10 +26,12 @@ queue_t *q_new()
 {
     queue_t *q =  malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
-    if q == NULL
-        return NULL
+    if (q == NULL)
+        return NULL;
     list_ele_t *dummy;
     dummy = malloc(sizeof(list_ele_t));
+    if (dummy == NULL)
+      return NULL;
     dummy->value = NULL;
     dummy->next = NULL;
     q->head = dummy;
@@ -42,15 +44,19 @@ queue_t *q_new()
 void q_free(queue_t *q)
 {
     /* How about freeing the list elements and the strings? */
-    while (q->next != NULL)
+    if (q != NULL && q->head != q->back)
     {
-      /* code */
+      while (q->head != NULL)
+      {
       list_ele_t *i = q->head;
       q->head = i->next;
+      free(i->value);
       free(i);
+      }
+      /* Free queue structure */
+      free(q);
     }
-    /* Free queue structure */
-    free(q);
+    
 }
 
 /*
@@ -62,16 +68,25 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
     /* What should you do if the q is NULL? */
     if (q == NULL)
       return false;
+    
+    list_ele_t *newh;
     newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
     if (newh == NULL)
       return false;
-    newh->value = s;
+    
+    /* Don't forget to allocate space for the string and copy it */
+    newh->value = malloc(sizeof(char) * strlen(s)+1);
+    /* What if either call to malloc returns NULL? */
+    if (newh->value == NULL)
+    {
+      free(newh);
+      return false;
+    }
+
+    strcpy(newh->value,s);
     newh->next = q->head;
     q->head = newh;
     q->size += 1;
@@ -96,12 +111,18 @@ bool q_insert_tail(queue_t *q, char *s)
     new_dummy = malloc(sizeof(list_ele_t));
     if (new_dummy == NULL)
       return false;
+    
     new_dummy->value = NULL;
     new_dummy->next = NULL;
-    q->back->value = s;
+    q->back->value = malloc(sizeof(char) * strlen(s)+1);
+    if (q->back->value == NULL)
+      return false;
+  
+    strcpy(q->back->value,s);
     q->back->next = new_dummy;
     q->back = new_dummy;
     q->size += 1;
+    
     return true;
 }
 
@@ -118,11 +139,18 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
     /* You need to fix up this code. */
     if (q == NULL || q->head == q->back)
       return false;
-    strncpy(sp,q->head->value,bufsize-1);
-    sp[bufsize] = NULL;
-    list_ele_t *i = q->head;
+    if (sp != NULL)
+    {
+      strncpy(sp,q->head->value,bufsize-1);
+      sp[bufsize-1] = '\0';
+    }
+    
+    list_ele_t *oldh = q->head;
     q->head = q->head->next;
-    free(i);
+
+    free(oldh->value);
+    free(oldh);
+
     q->size -= 1;
     return true;
 }
@@ -135,7 +163,10 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return q->size;
+    if (q == NULL || q->head == q->back)
+      return 0;
+    else
+      return q->size;
 }
 
 /*
@@ -148,15 +179,18 @@ int q_size(queue_t *q)
 void q_reverse(queue_t *q)
 {
     /* You need to write the code for this function */
+    if (q == NULL || q->head == q->back)
+      return 0;
     list_ele_t *point_array[q->size];
-    for (int i = 0; i <= (q->size - 1); i++)
+    int i;
+    for (i = 0; i <= (q->size - 1); i++)
     {
       point_array[i] = q->head;
       q->head = q->head->next;
     }
     /*when loop fininsh q->head point to dummy node*/
     q->head = point_array[q->size -1];
-    for (int i = 1; i <= (q->size -1); i++)
+    for (i = 1; i <= (q->size -1); i++)
     {
       point_array[i]->next = point_array[i-1];
     }
